19c3ae614d3182a3c5b07d28522d9124ed80c24b
Peter Askey <Peter_Askey@waters.com>
2019-09-11 13:50:26 +0100

    [PA Commit] Convert String to string in line with standard c# convention. Use Pascal Case for method names

--- exercises/csharp/TriviaGame/TriviaGame/GameRunner.cs
+++ exercises/csharp/TriviaGame/TriviaGame/GameRunner.cs
@@ -16,24 +16,24 @@ namespace TriviaGame
         {
             Game aGame = new Game();
 
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
 
             Random rand = new Random(Int32.Parse(args[0]));
 
             do
             {
 
-                aGame.roll(rand.Next(5) + 1);
+                aGame.Roll(rand.Next(5) + 1);
 
                 if (rand.Next(9) == 7)
                 {
-                    notAWinner = aGame.wrongAnswer();
+                    notAWinner = aGame.WrongAnswer();
                 }
                 else
                 {
-                    notAWinner = aGame.wasCorrectlyAnswered();
+                    notAWinner = aGame.WasCorrectlyAnswered();
                 }
 
 
