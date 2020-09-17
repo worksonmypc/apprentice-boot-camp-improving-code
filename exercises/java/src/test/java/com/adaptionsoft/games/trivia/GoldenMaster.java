44112f9faaac9ab126b8315260aa2831e9f61544
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2020-09-17 18:20:29 +0100

    Specify file encoding explicitly
    This will ensure that in the unlikely event that someone commits their golden master files, or shares their files, then the file encoding won’t vary by platform.

--- exercises/java/src/test/java/com/adaptionsoft/games/trivia/GoldenMaster.java
+++ exercises/java/src/test/java/com/adaptionsoft/games/trivia/GoldenMaster.java
@@ -5,18 +5,21 @@ import com.adaptionsoft.games.trivia.runner.GameRunner;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class GoldenMaster {
     private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private static final PrintStream originalOut = System.out;
-
+    public static final Charset BYTE_ENCODING = StandardCharsets.UTF_8;
 
     public static String getResult(int i) {
         String result = null;
         try {
-            result = new String(Files.readAllBytes(Paths.get("src","test","resources",String.format("output%d.txt", i))));
+            result = new String(Files.readAllBytes(Paths.get("src","test","resources",String.format("output%d.txt", i))), BYTE_ENCODING);
         } catch (IOException e) {
             result = generateExpectedResult(i);
         }
@@ -25,7 +28,12 @@ public class GoldenMaster {
 
     private static String generateExpectedResult(int i){
         String result = null;
-        System.setOut(new PrintStream(outContent));
+        try {
+            System.setOut(new PrintStream(outContent, true, BYTE_ENCODING.name()));
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("The encoding used to convert the game text into bytes and back could not be found. This will not happen with standard encodings.");
+            e.printStackTrace();
+        }
 
         GameRunner.main(new String[]{Integer.toString(i)});
 
